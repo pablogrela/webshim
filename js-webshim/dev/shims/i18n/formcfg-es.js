587ecd78e5aa89149cc918912a2582408eeb0904
pablogrela <pablogp_9@hotmail.com>
2015-06-13 17:06:35 +0200

    Fix Spanish i18n and add Galician i18n

--- js-webshim/dev/shims/i18n/formcfg-es.js
+++ js-webshim/dev/shims/i18n/formcfg-es.js
@@ -1,65 +1,77 @@
-webshims.validityMessages.es = {
-	"typeMismatch": {
-		"email": "Por favor, introduzca una dirección de correo.",
-		"url": "Por favor, introduzca una URL."
-	},
-	"badInput": {
-		"number": "Valor no válido",
-		"date": "Valor no válido",
-		"time": "Valor no válido",
-		"range": "Valor no válido",
-		"datetime-local": "Valor no válido"
-	},
-	"tooLong": "Valor no válido",
-	"patternMismatch": "Por favor, ajústese al formato solicitado: {%title}.",
-	"valueMissing": {
-		"defaultMessage": "Por favor, rellene este campo.",
-		"checkbox": "Por favor, marque esta casilla si desea continuar.",
-		"select": "Por favor, seleccione un elemento de la lista.",
-		"radio": "Por favor, seleccione una de estas opciones."
-	},
-	"rangeUnderflow": {
-		"defaultMessage": "El valor debe superior o igual a {%min}.",
-		"date": "El valor debe superior o igual a {%min}.",
-		"time": "El valor debe superior o igual a {%min}.",
-		"datetime-local": "El valor debe superior o igual a {%min}."
-	},
-	"rangeOverflow": {
-		"defaultMessage": "El valor debe inferior o igual a {%max}.",
-		"date": "El valor debe inferior o igual a {%max}.",
-		"time": "El valor debe inferior o igual a {%max}.",
-		"datetime-local": "El valor debe inferior o igual a {%max}."
-	},
-	"stepMismatch": "Valor no válido"
-};
-webshims.formcfg.es = {
-	numberFormat: {
-		".": ".",
-		",": ","
-	},
-	numberSigns: '.',
-	dateSigns: '/',
-	timeSigns: ":. ",
-	dFormat: "/",
-	patterns: {
-		d:  'dd/mm/yy'
-	},
-	date: {
-		closeText: 'Cerrar',
-		prevText: '&#x3C;Ant',
-		nextText: 'Sig&#x3E;',
-		currentText: 'Hoy',
-		monthNames: ['Enero','Febrero','Marzo','Abril','Mayo','Junio',
-		'Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
-		monthNamesShort: ['Ene','Feb','Mar','Abr','May','Jun',
-		'Jul','Ago','Sep','Oct','Nov','Dic'],
-		dayNames: ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado'],
-		dayNamesShort: ['Dom','Lun','Mar','Mié','Juv','Vie','Sáb'],
-		dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
-		weekHeader: 'Sm',
-		firstDay: 1,
-		isRTL: false,
-		showMonthAfterYear: false,
-		yearSuffix: ''
-	}
-};
+		webshims.validityMessages.es = {
+			typeMismatch : {
+				email : "Por favor, introduzca una dirección de correo.",
+				url : "Por favor, introduzca una URL."
+			},
+			badInput : {
+				number : "Por favor ingresa un número",
+				date : "Por favor ingresa una fecha",
+				time : "Por favor ingresa una hora",
+				range : "Valor no válido",
+				"datetime-local" : "Por favor ingresa una fecha con una hora"
+			},
+			validatevalue : "aqui",
+			dependentValidation : "aqui",
+			tooLong : "Por favor ingresa como máximo {%maxlength} caracteres, tú has ingresado solo {%valueLen}.",
+			tooShort : "El mínimo de caracteres es {%minlength}, tú solo ingresaste {%valueLen}.",
+			patternMismatch : "{%title}",
+			valueMissing : {
+				defaultMessage : "Por favor, rellene este campo.",
+				checkbox : "Por favor, marque esta casilla si desea continuar.",
+				select : "Por favor, seleccione un elemento de la lista.",
+				radio : "Por favor, seleccione una de estas opciones."
+			},
+			rangeUnderflow : {
+				defaultMessage : "El valor debe ser superior o igual a {%min}.",
+				date : "El valor debe ser superior o igual a {%min}.",
+				time : "El valor debe ser superior o igual a {%min}.",
+				"datetime-local" : "El valor debe ser superior o igual a {%min}."
+			},
+			rangeOverflow : {
+				defaultMessage : "El valor debe ser inferior o igual a {%max}.",
+				date : "El valor debe ser inferior o igual a {%max}.",
+				time : "El valor debe ser inferior o igual a {%max}.",
+				"datetime-local" : "El valor debe inferior o igual a {%max}."
+			},
+			stepMismatch : "Valor no v\xe1lido"
+		}, webshims.formcfg.es = {
+			numberFormat : {
+				"." : ".",
+				"," : ","
+			},
+			numberSigns : ".",
+			dateSigns : "/",
+			timeSigns : ":. ",
+			dFormat : "/",
+			month : {
+				currentText : "Este mes"
+			},
+			patterns : {
+				d : "dd/mm/yy"
+			},
+			time : {
+				currentText : "Ahora"
+			},
+			date : {
+				closeText : "Cerrar",
+				clear : "Borrar",
+				prevText : "&#x3C;Ant",
+				nextText : "Sig&#x3E;",
+				currentText : "Hoy",
+				monthNames : [ "Enero", "Febrero", "Marzo", "Abril", "Mayo",
+						"Junio", "Julio", "Agosto", "Septiembre", "Octubre",
+						"Noviembre", "Diciembre" ],
+				monthNamesShort : [ "Ene", "Feb", "Mar", "Abr", "May", "Jun",
+						"Jul", "Ago", "Sep", "Oct", "Nov", "Dic" ],
+				dayNames : [ "Domingo", "Lunes", "Martes", "Mi\xe9rcoles",
+						"Jueves", "Viernes", "S\xe1bado" ],
+				dayNamesShort : [ "Dom", "Lun", "Mar", "Mi\xe9", "Juv", "Vie",
+						"S\xe1b" ],
+				dayNamesMin : [ "Do", "Lu", "Ma", "Mi", "Ju", "Vi", "S\xe1" ],
+				weekHeader : "Sm",
+				firstDay : 1,
+				isRTL : !1,
+				showMonthAfterYear : !1,
+				yearSuffix : ""
+			}
+		};
